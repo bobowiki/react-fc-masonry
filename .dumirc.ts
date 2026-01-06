2739ccdc8bdf6c7affa241b9400f209311a79bda
yikai <yikai@didiglobal.com>
2026-01-06 18:05:06 +0800

    增加静态文件打包-github静态资源路近

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/react-fc-masonry/',
+  publicPath: '/react-fc-masonry/',
   themeConfig: {
-    name: 'react-masonry-fn',
+    name: 'react-fc-masonry',
+    socialLinks: {
+      github: 'https://github.com/bobowiki/react-virtual-list',
+    },
   },
 });
